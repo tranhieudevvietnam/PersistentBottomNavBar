b0ec331b427f42c66633cac5b2310ae21917e378
tranhieudevvietnam <tranhieu.dev.vietnam@gmail.com>
2021-07-09 23:51:46 +0700

    Update style-7-bottom-nav-bar.widget.dart

--- lib/nav-bar-styles/style-7-bottom-nav-bar.widget.dart
+++ lib/nav-bar-styles/style-7-bottom-nav-bar.widget.dart
@@ -8,110 +8,116 @@ class BottomNavStyle7 extends StatelessWidget {
     this.navBarEssentials = const NavBarEssentials(items: null),
   });
 
-  Widget _buildItem(
-      PersistentBottomNavBarItem item, bool isSelected, double? height) {
+  Widget _buildItem(PersistentBottomNavBarItem item, bool isSelected,
+      double? height, {context}) {
     return this.navBarEssentials!.navBarHeight == 0
         ? SizedBox.shrink()
         : AnimatedContainer(
-            width: isSelected ? 120 : 50,
-            height: height! / 1.6,
-            duration:
-                this.navBarEssentials!.itemAnimationProperties?.duration ??
-                    Duration(milliseconds: 400),
-            curve: this.navBarEssentials!.itemAnimationProperties?.curve ??
-                Curves.ease,
-            padding: EdgeInsets.all(item.contentPadding),
-            decoration: isSelected
-                ? BoxDecoration(
+      // width: isSelected ? 120 : 50,
+      width: isSelected ? MediaQuery
+          .of(context)
+          .size
+          .width / 2 : 50,
+      height: height! / 1.6,
+      duration:
+      this.navBarEssentials!.itemAnimationProperties?.duration ??
+          Duration(milliseconds: 400),
+      curve: this.navBarEssentials!.itemAnimationProperties?.curve ??
+          Curves.ease,
+      padding: EdgeInsets.all(item.contentPadding),
+      decoration: isSelected
+          ? BoxDecoration(
+        color: isSelected
+            ? item.activeColorPrimary
+            : this
+            .navBarEssentials!
+            .backgroundColor!
+            .withOpacity(0.0),
+        borderRadius: BorderRadius.all(Radius.circular(100)),
+      )
+          : BoxDecoration(
+        color: isSelected
+            ? item.activeColorPrimary
+            : this
+            .navBarEssentials!
+            .backgroundColor!
+            .withOpacity(0.0),
+        borderRadius: BorderRadius.all(Radius.circular(100)),
+      ),
+      child: Container(
+        alignment: Alignment.center,
+        height: height / 1.6,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 8),
+              child: IconTheme(
+                data: IconThemeData(
+                    size: item.iconSize,
                     color: isSelected
+                        ? (item.activeColorSecondary == null
                         ? item.activeColorPrimary
-                        : this
-                            .navBarEssentials!
-                            .backgroundColor!
-                            .withOpacity(0.0),
-                    borderRadius: BorderRadius.all(Radius.circular(100)),
-                  )
-                : BoxDecoration(
-                    color: isSelected
+                        : item.activeColorSecondary)
+                        : item.inactiveColorPrimary == null
                         ? item.activeColorPrimary
-                        : this
-                            .navBarEssentials!
-                            .backgroundColor!
-                            .withOpacity(0.0),
-                    borderRadius: BorderRadius.all(Radius.circular(100)),
-                  ),
-            child: Container(
-              alignment: Alignment.center,
-              height: height / 1.6,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.only(right: 8),
-                    child: IconTheme(
-                      data: IconThemeData(
-                          size: item.iconSize,
-                          color: isSelected
-                              ? (item.activeColorSecondary == null
-                                  ? item.activeColorPrimary
-                                  : item.activeColorSecondary)
-                              : item.inactiveColorPrimary == null
-                                  ? item.activeColorPrimary
-                                  : item.inactiveColorPrimary),
-                      child: isSelected
-                          ? item.icon
-                          : item.inactiveIcon ?? item.icon,
-                    ),
-                  ),
-                  item.title == null
-                      ? SizedBox.shrink()
-                      : isSelected
-                          ? Flexible(
-                              child: Material(
-                                type: MaterialType.transparency,
-                                child: FittedBox(
-                                    child: Text(
-                                  item.title!,
-                                  style: item.textStyle != null
-                                      ? (item.textStyle!.apply(
-                                          color: isSelected
-                                              ? (item.activeColorSecondary ==
-                                                      null
-                                                  ? item.activeColorPrimary
-                                                  : item.activeColorSecondary)
-                                              : item.inactiveColorPrimary))
-                                      : TextStyle(
-                                          color:
-                                              (item.activeColorSecondary == null
-                                                  ? item.activeColorPrimary
-                                                  : item.activeColorSecondary),
-                                          fontWeight: FontWeight.w400,
-                                          fontSize: 12.0),
-                                )),
-                              ),
-                            )
-                          : SizedBox.shrink()
-                ],
+                        : item.inactiveColorPrimary),
+                child: isSelected
+                    ? item.icon
+                    : item.inactiveIcon ?? item.icon,
               ),
             ),
-          );
+            item.title == null
+                ? SizedBox.shrink()
+                : isSelected
+                ? Flexible(
+              child: Material(
+                type: MaterialType.transparency,
+                child: FittedBox(
+                    child: Text(
+                      item.title!,
+                      style: item.textStyle != null
+                          ? (item.textStyle!.apply(
+                          color: isSelected
+                              ? (item.activeColorSecondary ==
+                              null
+                              ? item.activeColorPrimary
+                              : item.activeColorSecondary)
+                              : item.inactiveColorPrimary))
+                          : TextStyle(
+                          color:
+                          (item.activeColorSecondary == null
+                              ? item.activeColorPrimary
+                              : item.activeColorSecondary),
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12.0),
+                    )),
+              ),
+            )
+                : SizedBox.shrink()
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    print("${this.navBarEssentials!.navBarHeight}");
     return Container(
       width: double.infinity,
       height: this.navBarEssentials!.navBarHeight,
       padding: EdgeInsets.only(
           top: this.navBarEssentials!.padding?.top ??
               this.navBarEssentials!.navBarHeight! * 0.15,
-          left: this.navBarEssentials!.padding?.left ??
-              MediaQuery.of(context).size.width * 0.07,
-          right: this.navBarEssentials!.padding?.right ??
-              MediaQuery.of(context).size.width * 0.07,
+          // left: this.navBarEssentials!.padding?.left ??
+          //     MediaQuery.of(context).size.width * 0.07,
+          // right: this.navBarEssentials!.padding?.right ??
+          //     MediaQuery.of(context).size.width * 0.07,
           bottom: this.navBarEssentials!.padding?.bottom ??
-              this.navBarEssentials!.navBarHeight! * 0.15),
+              this.navBarEssentials!.navBarHeight! * 0.15
+      ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -131,9 +137,11 @@ class BottomNavStyle7 extends StatelessWidget {
               child: Container(
                 color: Colors.transparent,
                 child: _buildItem(
-                    item,
-                    this.navBarEssentials!.selectedIndex == index,
-                    this.navBarEssentials!.navBarHeight),
+                  item,
+                  this.navBarEssentials!.selectedIndex == index,
+                  this.navBarEssentials!.navBarHeight,
+                  context: context,
+                ),
               ),
             ),
           );
